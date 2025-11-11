45f77a95627c44c29808387a9e07e711580797af
Luan Freitas <luandkg@gmail.com>
2025-11-11 19:41:53 -0300

    feat(app) : simplifica a inicialização dos apps

---
+++ src/app/Apps.java
@@ -0,0 +1,22 @@
+package app;
+
+import oe.Inicializador;
+
+public class Apps {
+
+    public static final int APP_TRONARKO = 0;
+    public static final int APP_HARREMPLUZ = 1;
+    public static final int APP_ALARME = 2;
+
+    public static void INICIAR(int app){
+
+        if (app == APP_TRONARKO) {
+            Inicializador.criar("tronarko", 1500, 1020, "editor.png", new TronarkoCena());
+        } else if (app == APP_HARREMPLUZ) {
+            Inicializador.criar("Harrempluz", 1500, 1020, "editor.png", new HarrempluzCena());
+        } else if (app == APP_ALARME) {
+            Inicializador.criar("Alarme", 900, 600, "res/editor.png", new Alarme());
+        }
+
+    }
+}
--- src/app/TronarkoMain.java
+++ src/app/TronarkoMain.java
@@ -1,7 +1,6 @@
 package app;
 
 import khronum.MinhaAgenda;
-import oe.Inicializador;
 import tronarko.Tronarko;
 
 import java.awt.*;
@@ -14,22 +13,7 @@ public class TronarkoMain {
         eMinhaAgenda.init_2021();
 
 
-        int App = 0;
-
-        if (App == 0) {
-
-            Inicializador.criar("tronarko", 1500, 1020, "editor.png", new TronarkoCena());
-
-        } else if (App == 1) {
-
-            Inicializador.criar("Harrempluz", 1500, 1020, "editor.png", new HarrempluzCena());
-
-        } else if (App == 2) {
-
-            Inicializador.criar("Alarme", 900, 600, "res/editor.png", new Alarme());
-
-        }
-
+        Apps.INICIAR(Apps.APP_TRONARKO);
 
 
         Tronarko a = new Tronarko();
@@ -38,8 +22,8 @@ public class TronarkoMain {
         System.out.println("\t27/07/2021 :: " + a.getData("27/07/2021").toString());
         System.out.println("\t27/07/2022 :: " + a.getData("27/07/2022").toString());
 
-     //   System.out.println("\tD1 :: " + a.getIntervalo("D1", a.getData("27/07/2020"), a.getData("27/07/2021")).getSuperarkos());
-       // System.out.println("\tD2 :: " + a.getIntervalo("D2", a.getData("27/07/2021"), a.getData("27/07/2022")).getSuperarkos());
+        //   System.out.println("\tD1 :: " + a.getIntervalo("D1", a.getData("27/07/2020"), a.getData("27/07/2021")).getSuperarkos());
+        // System.out.println("\tD2 :: " + a.getIntervalo("D2", a.getData("27/07/2021"), a.getData("27/07/2022")).getSuperarkos());
 
         System.out.println("\tEpoca :: " + a.getTozte().getEpoca().toString() + " em " + a.getTozte().getTronarkosDaEpoca());
 
